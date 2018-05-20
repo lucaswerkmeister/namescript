d5b8304b1a5ffc116a25ce54b36e7b6578390196
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2018-05-20 14:36:56 +0200

    Add namescript-browser.js and namescript.js
    namescript-browser.js loads namescript-lib.js and namescript-data.json
    from Wikidata user subpages, configures namescript and calls the entry
    point for the current entity. Instead of directly doing the edits, it
    adds a “button” to the P31 statement.

---
+++ namescript-browser.js
@@ -0,0 +1,60 @@
+const sourceUser = 'Lucas_Werkmeister'; // **IF YOU COPY THIS USER SCRIPT TO YOUR OWN PAGE, REPLACE THIS WITH YOUR OWN USER NAME**
+
+$.when(
+	$.get('https://www.wikidata.org/w/index.php?title=User:' + sourceUser + '/namescript-lib.js&action=raw&ctype=text/javascript'),
+	$.get('https://www.wikidata.org/w/index.php?title=User:' + sourceUser + '/namescript-data.json&action=raw&ctype=application/json'),
+	$.Deferred(function(deferred) {
+		mw.hook('wikibase.entityPage.entityLoaded').add(deferred.resolve);
+	})
+).then(function(namescriptLibXhr, namescriptDataXhr, entity) {
+	const api = new mw.Api();
+	namescript.config = {
+		apiRequest: /* async */ function(params, withEditToken) {
+			// mw.Api uses jQuery.Promise, so we have to map to native Promise manually
+			return new Promise(function(resolve, reject) {
+				let $promise;
+				if (withEditToken) {
+					$promise = api.postWithEditToken(params);
+				} else {
+					$promise = api.get(params);
+				}
+				$promise.then(resolve, reject);
+			});
+		},
+		clearDescriptions: false,
+		prepareAdd: async function(add) {
+			$('#P31 .wikibase-snakview-variation-valuesnak a').after($('<div  />', {
+				css: {
+					"font-weight": "bold",
+					"cursor": "pointer"
+				},
+				text: namescript.translate('add-button'),
+				click: add
+			}));
+		},
+		lang: mw.config.get('wgUserLanguage'),
+		infoActive: function(message) {
+			mw.notify(message, {
+				title: namescript.translate('title'),
+				tag: 'Name script'
+			});
+		},
+		errorActive: function(message) {
+			mw.notify(message, {
+				title: namescript.translate('title'),
+				tag: 'Name script',
+				type: 'error'
+			});
+		},
+		errorP31: function(message) {
+			$('#P31 .wikibase-snakview-variation-valuesnak a').after($('<div />', {
+				text: message,
+				css: {
+					"color": "#f00"
+				}
+			}));
+		}
+	};
+	namescript.data = namescriptDataXhr[0];
+	namescript.start(entity);
+});
---
+++ namescript.js
@@ -0,0 +1,11 @@
+if (typeof window === 'object' &&
+	typeof document === 'object' &&
+	typeof mw === 'object' &&
+	typeof $ === 'function') {
+	const sourceUser = 'Lucas_Werkmeister'; // **IF YOU COPY THIS USER SCRIPT TO YOUR OWN PAGE, REPLACE THIS WITH YOUR OWN USER NAME**
+	$.get('https://www.wikidata.org/w/index.php?title=User:' + sourceUser + '/namescript-browser.js&action=raw&ctype=text/javascript');
+} else if (typeof require === 'function') {
+	require('./namescript-cli.js');
+} else {
+	console.error('Could not detect environment!');
+}
