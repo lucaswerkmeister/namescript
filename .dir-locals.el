79b00a47cfcfc519ea0959570ae8ee0db5e78820
Lucas Werkmeister <mail@lucaswerkmeister.de>
2018-06-04 23:33:12 +0200

    Fix .dir-locals.el
    It took ‘M-x add-dir-local-variable’ for me to realize that I’d
    misspelled the mode name -.-
    
    But the version added by that function is nicer anyways. And also
    configure two additional variables.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,7 @@
-((javascript-mode . ((require-final-newline . nil))))
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((js-mode
+  (require-final-newline . nil)
+  (tab-width . 4)
+  (indent-tabs-mode . t)))
