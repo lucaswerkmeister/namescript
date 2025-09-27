lucaswerkmeister/namescript

commit 040d63b2a3112358dadee9d8ed5711cb6b842d77

Account for implicit 'en' fallback

namescript-lib.js
@@ -286,7 +286,8 @@ namescript = {
 		const codesToKeep = new Set();
 		const codesToSet = new Set(['mul']);
 
-		const maxFallbacks = Math.max(...Object.values(languageinfo).map(({fallbacks}) => fallbacks.length));
+		// maximum length of fallback chains (plus one to account for implicit 'en' fallback)
+		const maxFallbacks = Math.max(...Object.values(languageinfo).map(({fallbacks}) => fallbacks.length + 1));
 
 		for (let i = 0; i < maxFallbacks * 2 && todoList.length > 0; i++) {
 			todos:
@@ -303,7 +304,7 @@ namescript = {
 					continue;
 				}
 				fallbacks:
-				for (const fallbackCode of languageinfo[languageCode].fallbacks) {
+				for (const fallbackCode of [...languageinfo[languageCode].fallbacks, 'en']) {
 					const fallbackLabel = entity.labels[fallbackCode]?.value;
 					if (fallbackLabel && fallbackLabel !== name) {
 						// set this label to the name so it does not fall back to a different label

commit dfea48e3fdd758bb86d1962a19ce4f1d34e216a1

Validate “cycle between fallback languages” handling
This code near the end of prepareLabels() is “dangerous”, in a way: its
existence can very easily mask bugs in the rest of the function, by
still removing any labels in the TODO list, even though the TODO list
should be almost empty at this point. This means that tests which check
that labels are removed might appear to work only due to this code,
turning it from a fix for a minor edge case into the “load bearing” part
of the function – this was previously seen in commit 14c0e8a692, and
now happened again (bug introduced in commit 040d63b2a3).

Avoid this trap by adding checks: if the TODO list contains anything
other than a cycle between fallback languages that are all set to the
name (i.e. if any fallback language is not set to be removed – either as
part of the codesToRemove or as part of the todoList – or has a label
different from the name), crash out of the whole function with an Error.

This revealed the bug introduced in commit 040d63b2a3: by including 'en'
in the list of fallback languages, we broke the main “todos” loop for
'en' as the languageCode itself, which now looked like it fell back to a
language ('en') where it wasn’t known if it would be removed or not (the
knownToFallBackToMul = false case); this in turn meant any language
falling back to en (that is: any language) couldn’t safely be removed in
the main “todos” loop either. Fix it by simply skipping the “fallbacks”
loop in this case (or, hypothetically, if any MediaWiki language code
included itself in its list of fallbacks, though I doubt that happens).

namescript-lib.js
@@ -306,6 +306,10 @@ namescript = {
 				let knownToFallBackToMul = true;
 				fallbacks:
 				for (const fallbackCode of [...languageinfo[languageCode].fallbacks, 'en']) {
+					if (fallbackCode === languageCode) {
+						// not an actual fallback (probably caused by the insertion of 'en' above)
+						continue fallbacks;
+					}
 					const fallbackLabel = entity.labels[fallbackCode]?.value;
 					if (fallbackLabel && fallbackLabel !== name) {
 						// set this label to the name so it does not fall back to a different label
@@ -346,8 +350,22 @@ namescript = {
 		}
 
 		for (const languageCode of todoList) {
-			// anything left in the TODO list must be a cycle between fallback languages that are all set to the name, so remove it
-			// FIXME: probably worth validating that assumption in the code, should be easy enough after all
+			// anything left in the TODO list must be a cycle between fallback languages that are all set to the name
+			for (const fallbackCode of [languageCode, ...languageinfo[languageCode].fallbacks, 'en']) {
+				const fallbackLabel = entity.labels[fallbackCode]?.value;
+				if (!codesToRemove.has(fallbackCode) && !todoList.includes(fallbackCode)) {
+					throw new Error(
+						'Namescript error: fallback language ' + fallbackCode + ' of language ' + languageCode +
+							' is not going to be removed but has no clear reason to be kept either'
+					);
+				}
+				if (fallbackLabel && fallbackLabel !== name) {
+					throw new Error(
+						'Namescript error: fallback language ' + fallbackCode + ' of language ' + languageCode +
+							' is not going to be removed but has a different label'
+					);
+				}
+			}
 			codesToRemove.add(languageCode);
 		}
 