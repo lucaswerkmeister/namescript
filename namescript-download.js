1e989125ccbfbf3a0ccc921c9882d2cc08462400
Lucas Werkmeister <mail@lucaswerkmeister.de>
2025-10-11 17:50:30 +0200

    Update namescript-download.js
    Work around gesinn-it-pub/mwbot#62, and also adjust the code for the new
    API response structure for revisions. I’m guessing this has been broken
    since I bumped the mwbot version in commit 5f38290c70 (evidently without
    testing namescript-download.js at the time).

--- namescript-download.js
+++ namescript-download.js
@@ -7,10 +7,14 @@ async function main() {
 	});
 	const sourceUser = 'Harmonia_Amanda';
 
+	// work around https://github.com/gesinn-it-pub/mwbot/issues/62
+	await bot.getSiteinfo();
+	bot.mwversion = require('semver').coerce(bot.state.generator);
+
 	for (const file of ['namescript-lib.js', 'namescript-data.json', 'namescript-browser.js', 'namescript.js']) {
 		const response = await bot.read('User:' + sourceUser + '/' + file),
 			  page = response.query.pages[Object.keys(response.query.pages)[0]];
-		var content = page.revisions[0]['*'];
+		var content = page.revisions[0].slots.main['*'];
 		content = content.replace(sourceUser, 'NAMESCRIPT_SOURCE_USER');
 		fs.writeFileSync(file, content, 'utf8');
 	}
